79f8b873aa4ba89ba06a504e0721c8acf0eca512
level42ca <level42ca@users.noreply.github.comm>
2017-04-15 20:51:02 -0400

    numbergame.py - Second Commit

--- number-game.py
+++ number-game.py
@@ -2,37 +2,43 @@
 import random
 
 def game():
-    # Generate a Random number between 1 - 10
-    secret_num = random.randint(1, 10)
-    
-    # Keeps track of the number of guess'
-    counter = []
-    
-    while len(counter) < 5:
-    
+    # Generate a Random number between 1 - 1000
+    secret_num = random.randint(1, 1000)
+
+    # Keeps track of the number of guesses in an array
+    guesses = []
+
+    # Checks the length of "counter" to see if there are less than 20 guesses
+    while len(guesses) < 20:
+
         # Get a number guess from the player
         try:
-            guess = int(input("Guess a number between 1 and 10: \n -->: "))
+            guess = int(input("Guess a number between 1 and 1000: \n -->: "))
         except ValueError:
             print("That is not a number")
         else:
             # Compare the guess to the secret number
             if guess == secret_num:
-                print("You got it!, the secret number was {}.".format(secret_num))
+                print(" -->: You got it!, the secret number was {}.".format(secret_num))
                 break
+            # Print hit/miss to show user how close they are
             elif guess < secret_num:        
-                print("That's not it, the secret number is greater then what you've guessed.\n")
+                print(" -->: That's not it, the secret number is greater then what you've guessed.\n")
             elif guess > secret_num:        
-                print("That's not it, the secret number is less then what you've guessed.\n")
-            counter.append(guess)
-                
-                
-            # print hit/miss
+                print(" -->: That's not it, the secret number is less then what you've guessed.\n")
+            guesses.append(guess)
+
     else:
-        print("You lost, the secret number was {}.".format(secret_num))
+        print(" -->: You lost, the secret number was {}.".format(secret_num))
+
     play_again = input("Do you want to play again? Y/n ")
-    if play_again.lower() != "n":
+
+    if play_again.lower() != 'n':
+        print ""
+        print ""
+        print ""
         game()
     else:
         print("Bye!")
+        
 game()
